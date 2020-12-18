6109c4af37fab7cc3b2317bc0eaa01fe4b409637
Joakim Taule Kartveit <joakim.kartveit@edigard.no>
2020-12-18 14:45:41 +0100

    Changed title to Todo list App

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -8,17 +8,11 @@ interface Props {
 export const TodoListItem: React.FC<Props> = ({todo, toggleTodo}) => {
     return (
         <li>
-            <label
-                style={{textDecoration: todo.complete ? 'line-through' : undefined}}
-            >
-                <input
-                    type="checkbox"
-                    checked={todo.complete}
-                    onClick={() => {
-                        toggleTodo(todo);
-                    }}
-                />{' '}
-                {todo.text}
+            <label style={{textDecoration: todo.complete ? 'line-through' : undefined}}>
+                <input type="checkbox" checked={todo.complete} onClick={() => {
+                    toggleTodo(todo);
+                }}/>
+                {' '}{todo.text}
             </label>
         </li>
     );
