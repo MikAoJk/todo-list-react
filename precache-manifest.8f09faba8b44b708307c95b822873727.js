94b645347c3498db05f79fb2f2120c7a2a6ec3cd
Joakim Taule Kartveit <joakim.kartveit@edigard.no>
2021-05-03 08:49:03 +0200

    Updates

---
+++ precache-manifest.8f09faba8b44b708307c95b822873727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361f57cfbdda2af6bca9bc2a7be75678",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "92320eaabdd56f90d910",
+    "url": "/todo-list-react/static/js/2.42c3c565.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo-list-react/static/js/2.42c3c565.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbe304542373717d937",
+    "url": "/todo-list-react/static/js/main.2f9403dd.chunk.js"
+  },
+  {
+    "revision": "d2ce1cbae4c6bd42d20e",
+    "url": "/todo-list-react/static/js/runtime-main.dc5b2452.js"
+  }
+]);
