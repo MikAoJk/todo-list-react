86f2f5224c0c3d2b20abc4b7c4ad4026fb0df8c4
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2025-07-22 18:53:46 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {TodoList} from './TodoList';
 import {AddTodoForm} from './AddTodoForm';
+import './App.css';
 
 const initialTodos: Todo[] = [
     {
